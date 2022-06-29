79204598ab416b87e8bda75791a45d60a33240aa
jonghyeong <jonghyeong.lee@furiosa.ai>
2022-06-30 04:25:39 +0900

    Update max_histogram

--- panoptic-deeplab/tools_d2/minmax_histogram.py
+++ panoptic-deeplab/tools_d2/minmax_histogram.py
@@ -8,6 +8,7 @@ import torch
 from random import sample
 import onnx
 from onnx import numpy_helper
+from scipy.stats import percentileofscore
 
 torch.manual_seed(1000)
 max_list = {'semantic':{'res5':[],'res3':[],'res2':[]},'instance':{'res5':[],'res3':[],'res2':[]}}
@@ -68,47 +69,48 @@ def get_q_max(model_quant):
     q_max_list = []
     temp = 0
     for i in range(0,len(q_params),2):
-        scale = q_params[i]
-        zero_pt = q_params[i+1]
+        scale = q_params[i+1]
+        zero_pt = q_params[i]
         q_max = (256-zero_pt)*scale
         q_max_list.append(q_max)
-        if i in [4,6,8,13,15,17]:
-            q_max_list.append(max(q_max_list[temp:i]))
-            temp = i
+        if i//2 in [4,6,8,13,15,17]:
+            q_max_list.append(max(q_max_list[temp-i//2:]))
+            temp = i//2
     
     return q_max_list
 
-
 #get percentile of q-scale
-
+def get_percentile(q_max_dict, max_list):
+    percentile = {'semantic':{'res5':[],'res3':[],'res2':[]},'instance':{'res5':[],'res3':[],'res2':[]}}
+    for key in max_list:
+        for res in max_list[key]:
+            for idx, max_dist in enumerate(max_list[key][res]):
+                p = percentileofscore(max_dist, q_max_dict[key][res][idx])
+                if p >= 100:
+                    percentile[key][res].append((p,q_max_dict[key][res][idx]/max(max_dist)))
+                else:
+                    percentile[key][res].append(p)
+    return percentile
 
 mean = np.repeat(np.array([[[128]]]),3,axis=0)
 std = np.repeat(np.array([[[128]]]),3,axis=0)
 sess = ort.InferenceSession("/root/ljh726/PanopticDeepLab/warboy/xception65_dsconv_4812_1024_2048/panoptic_hist.onnx")
 model_quant = "/root/ljh726/PanopticDeepLab/warboy/xception65_dsconv_4812_1024_2048/panoptic-int8-cal30_seed1000.onnx"
 
-#q_max_list = get_q_max(model_quant)
-#print(q_max_list)
-#q_max_dict = create_hist_dict(q_max_list)
-#print_hist(q_max_dict)
-
 
 args = get_parser().parse_args()
 cfg = setup_cfg(args)
 model = build_model(cfg)
 model.eval()
-dataset = glob.glob("datasets/cityscapes_origin/leftImg8bit/val/*/*.png")
-
+dataset = glob.glob("datasets/cityscapes/leftImg8bit/val/*/*.png")
 
 #initialize
 init_list(max_list)
 
 
+for idx, file in enumerate(dataset):
+    print("%dth file: "%idx,file)
 
-for idx, file in enumerate(sample(dataset,1)):
-    print(file)
-    if idx > 20:
-        break
     img = np.array(Image.open(file))
     #preprocess
     img = img[:,:,::-1]
@@ -120,12 +122,19 @@ for idx, file in enumerate(sample(dataset,1)):
     #get network outputs: sem_seg,center,offset,histograms
     model.network_only=True
     model.onnx = True
-    outputs1 = model([torch.as_tensor(img).to(model.device)])
+#    outputs1 = model([torch.as_tensor(img).to(model.device)])
 
     outputs2 = sess.run(None, {'image':img})
-    print(torch.max(outputs1[0]))
-    print(outputs2[0].max())
-    #histogram = create_hist_dict(outputs[3:])
-    #get_max(histogram)
+#    print(torch.max(outputs1[0]))
+#    print(outputs2[0].max())
+    histogram = create_hist_dict(outputs2[3:])
+    get_max(histogram)
 
 #print_hist(max_list)
+
+q_max_list = get_q_max(model_quant)
+q_max_dict = create_hist_dict(q_max_list)
+percentile = get_percentile(q_max_dict, max_list)
+print_hist(max_list)
+print_hist(q_max_dict)
+print_hist(percentile)
